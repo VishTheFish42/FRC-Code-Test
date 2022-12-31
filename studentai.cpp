e70d5f6f9093e8a15ec5daaab4b7679e332b40e7
VishTheFish42 <121592353+VishTheFish42@users.noreply.github.com>
2022-12-30 16:03:57 -0800

    Add files via upload
    Unislanded maze fully completed.
    
    I added an algorithm that checks if the mouse is in an uninterrupted 2x2 square and stops moving if it is.

--- studentai.cpp
+++ studentai.cpp
@@ -1,5 +1,7 @@
 
 #include "micromouseserver.h"
+#include <iostream>
+#include <unistd.h>
 
 void microMouseServer::studentAI()
 {
@@ -22,18 +24,88 @@ void microMouseServer::studentAI()
  * void printUI(const char *mesg);
 */
 
+    // determine which way to turn
+        // if can turn right then turn right
     if (!isWallRight()) {
         turnRight();
     }
-    else {
+    else { // otherwise if can only turn left then turn left
         if (isWallForward() && !isWallLeft()) {
             turnLeft();
         }
-        else if (isWallForward() && isWallLeft() && isWallRight()) {
+        else if (isWallForward() && isWallLeft() && isWallRight()) { // otherwise if you're at a dead end then make a u-turn
             turnLeft();
             turnLeft();
         }
     }
 
-    moveForward();
+    moveForward(); // move forward after the turn
+
+    // condition to check if the mouse has entered something that could be a 2x2 square
+        // only 8 possibilities of how mouse can enter the end zone but only two possibilities of circling the square
+            // OPTION 1: finish the square with left turns
+            // OPTION 2: finish the square with right turns
+
+    // OPTION 1
+    if (!isWallLeft() && isWallRight() && !isWallForward()) {
+        for (int i = 0; i < 3; i++) {
+            moveForward();
+            turnLeft();
+
+            if (!(!isWallLeft() && isWallRight() && !isWallForward())) {
+                turnLeft();
+                if (i == 0) {
+                    for (int j = i; j != -1; j--) {
+                        moveForward();
+                        turnLeft();
+                    }
+                    turnLeft();
+                }
+                else {
+                    for (int j = i; j != -1; j--) {
+                        moveForward();
+                        turnRight();
+                    }
+                    turnRight();
+                }
+                break;
+            }
+
+            if (i == 2) {
+                foundFinish();
+            }
+        }
+    }
+    // OPTION 2
+    else if (isWallLeft() && !isWallRight() && !isWallForward()) {
+        for (int i = 0; i < 3; i++) {
+            moveForward();
+            turnRight();
+
+            if (!(isWallLeft() && !isWallRight() && !isWallForward())) {
+                turnRight();
+                if (i == 0) {
+                    for (int j = i; j != -1; j--) {
+                        moveForward();
+                        turnRight();
+                    }
+                    turnRight();
+                }
+                else {
+                    for (int j = i; j != -1; j--) {
+                        moveForward();
+                        turnLeft();
+                    }
+                    turnLeft();
+                }
+                break;
+            }
+
+            if (i == 2) {
+                foundFinish();
+            }
+        }
+    }
+
+    // usleep(900000);
 }
